8deb20fb3d60de8ba225bf2f1eb78d4677110fbf
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-07-11 19:14:48 +0300

    input: gpio_keys: remove static modifier to make the variables modifiable using kmemhelper, add enable/disable module parameter and also make homekey_trg_cnt reflect the number of presses by modifying the comparison (thanks to tungstwenty)

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -453,8 +453,11 @@ static inline int64_t get_time_inms(void) {
 
 #define HOME_KEY_VAL	0xac
 extern void mdnie_toggle_negative(void);
-static int homekey_trg_cnt = 4;
-static int homekey_trg_ms = 300;
+int homekey_trg_cnt = 4;
+int homekey_trg_ms = 300;
+
+static int mdnie_shortcut_enabled = 1;
+module_param_named(mdnie_shortcut_enabled, mdnie_shortcut_enabled, int, S_IRUGO | S_IWUSR | S_IWGRP);
 
 static void gpio_keys_report_event(struct gpio_button_data *bdata)
 {
@@ -513,7 +516,7 @@ static void gpio_keys_report_event(struct gpio_button_data *bdata)
 #endif
 
 	//mdnie negative effect toggle by gm
-	if(button->code == HOME_KEY_VAL)
+	if((button->code == HOME_KEY_VAL) && mdnie_shortcut_enabled)
 	{
 		if(state) {
 			if (  get_time_inms() - homekey_lasttime < homekey_trg_ms) {
@@ -526,7 +529,7 @@ static void gpio_keys_report_event(struct gpio_button_data *bdata)
 			}
 		}
 		else {
-			if(homekey_count==homekey_trg_cnt)
+			if(homekey_count>=homekey_trg_cnt - 1)
 			{
 				mdnie_toggle_negative();
 				homekey_count = 0;
