5497b35b6d1ea08ebe80e07e0d6c22d9813db60f
Arve Hjønnevåg <arve@android.com>
2012-09-26 18:01:17 -0700

    staging: android: lowmemorykiller: Don't count reserved free memory
    The amount of reserved memory varies between devices. Subtract it
    here to reduce the amount of devices specific tuning needed for the
    minfree values.
    
    Change-Id: I466ae8b18f5972f6f6d8b5a7d8c4ae69660de53a
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -35,6 +35,7 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
+#include <linux/swap.h>
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
 #ifdef CONFIG_ZRAM_FOR_ANDROID
@@ -156,10 +157,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 #endif
 	int array_size = ARRAY_SIZE(lowmem_adj);
 #ifndef CONFIG_DMA_CMA
-	int other_free = global_page_state(NR_FREE_PAGES);
+	int other_free = global_page_state(NR_FREE_PAGES) -
+					totalreserve_page;
 #else
 	int other_free = global_page_state(NR_FREE_PAGES) -
-					global_page_state(NR_FREE_CMA_PAGES);
+					global_page_state(NR_FREE_CMA_PAGES) -
+					totalreserve_page;
 #endif
 	int other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
