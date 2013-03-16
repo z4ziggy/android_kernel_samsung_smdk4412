e65cfc3b4327b1f38ef5cec96e2a07ad52b6e458
DerTeufel <dominik-kassel@gmx.de>
2013-03-16 09:24:45 +0100

    Revert "trying to fix build error with: CONFIG_DMA_CMA=y"
    This reverts commit d40a49aba21d565ef979beac6a38ce9f9e2f1284.

--- mm/compaction-cma.c
+++ mm/compaction-cma.c
@@ -227,7 +227,6 @@ isolate_migratepages_range(struct zone *zone, struct compact_control *cc,
 	unsigned long last_pageblock_nr = 0, pageblock_nr;
 	unsigned long nr_scanned = 0, nr_isolated = 0;
 	struct list_head *migratelist = &cc->migratepages;
-	isolate_mode_t mode = ISOLATE_ACTIVE|ISOLATE_INACTIVE;
 
 	/*
 	 * Ensure that there are not too many pages isolated from the LRU
@@ -303,11 +302,8 @@ isolate_migratepages_range(struct zone *zone, struct compact_control *cc,
 			continue;
 		}
 
-		if (!cc->sync)
-			mode |= ISOLATE_ASYNC_MIGRATE;
-
 		/* Try isolate the page */
-		if (__isolate_lru_page(page, mode, 0) != 0)
+		if (__isolate_lru_page(page, ISOLATE_BOTH, 0) != 0)
 			continue;
 
 		VM_BUG_ON(PageTransCompound(page));
