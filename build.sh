322750ab16f3a59464111e96447785cbc232181a
DerTeufel <dominik-kassel@gmx.de>
2013-04-15 13:39:10 +0200

    more build stuff
    Change-Id: If3520eadba6a1f23c95b31c9ee3dcf0517d28584

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+build ()
+{
+    local target=$1
+    ./build_aosp.sh $target
+    ./build_sammy.sh $target
+}
+    
+targets=("$@")
+if [ 0 = "${#targets[@]}" ] ; then
+    targets=(t0lte n7100 i9300)
+fi
+
+START=$(date +%s)
+
+for target in "${targets[@]}" ; do 
+    build $target
+done
+
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
