106216c538162821eda4d8819434f17aaf70def5
DerTeufel <dominik-kassel@gmx.de>
2013-04-20 12:16:23 +0200

    cleaning
    Change-Id: I490d71df9586456fc9dac264c3667910dff61dda

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 15 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#15 SMP PREEMPT Tue Oct 9 06:16:59 CEST 2012"
-#define LINUX_COMPILE_BY "af"
-#define LINUX_COMPILE_HOST "mintvm"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
