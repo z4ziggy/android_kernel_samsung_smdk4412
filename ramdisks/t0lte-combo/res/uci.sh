3f403aa5a2f74388cd3db6f5a5dfb62745004080
DerTeufel <dominik-kassel@gmx.de>
2013-04-27 22:26:48 +0200

    Stweaks
    Change-Id: I96b69255a437538195938c9ea5410651ad5f9349

---
+++ ramdisks/t0lte-combo/res/uci.sh
@@ -0,0 +1,46 @@
+#!/sbin/busybox sh
+# universal configurator interface
+# by Gokhan Moral
+
+# You probably won't need to modify this file
+# You'll need to modify the files in /res/customconfig directory
+ACTION_SCRIPTS=/res/customconfig/actions;
+source /res/customconfig/customconfig-helper;
+
+# first, read defaults
+read_defaults;
+
+# read the config from the active profile
+read_config;
+
+UCI_PID=`pgrep "uci.sh"`;
+renice -n 15 -p $UCI_PID;
+
+case "${1}" in
+	rename)
+    	rename_profile "${2}" "${3}";
+    ;;
+	delete)
+    	delete_profile "${2}";
+    ;;
+	select)
+    	select_profile "${2}";
+    ;;
+	config)
+    	print_config;
+    ;;
+	list)
+    	list_profile;
+    ;;
+	apply)
+		apply_config;
+		write_config;
+	;;
+	restore)
+		apply_config;
+	;;
+	*)
+		. ${ACTION_SCRIPTS}/${1} ${1} ${2} ${3} ${4} ${5} ${6};
+		write_config;
+	;;
+esac;
