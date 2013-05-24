0a4233fe6a32333f565c47d8dddb9b3f9656ac72
DerTeufel <dominik-kassel@gmx.de>
2013-05-24 12:26:13 +0200

    this is working dualboot for aosp now
    Change-Id: I95d1d7410563dc8bfea669717cdbc86cca560274

--- ramdisks/t0lte-combo/sbin/mount_data.sh
+++
@@ -1,7 +0,0 @@
-#!/sbin/busybox sh
-BB="/sbin/busybox"
-
-$BB mkdir -p /.secondrom/media/.secondrom/data
-$BB mount --bind /.secondrom/media/.secondrom/data /data
-$BB mkdir /data/media
-$BB mount --bind /.secondrom/media /data/media
