9c56e1dd27f8b3ff8084c2c0b6e5f2427a3c3019
DerTeufel <dominik-kassel@gmx.de>
2013-10-18 16:28:54 +0200

    Revert "UKSM 0.1.2.2"
    This reverts commit d4fa463a46a20b85371cd68901e4f1a938aa9767.
    
    Conflicts:
    	include/linux/mm_types.h
    	mm/uksm.c
    
    Change-Id: I6839984188c8ac3c585a3989add96f628900737b

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -199,10 +199,6 @@ struct vm_area_struct {
 	struct mempolicy *vm_policy;	/* NUMA policy for the VMA */
 #endif
 
-
-#ifdef CONFIG_UKSM
-	struct vma_slot *uksm_vma_slot;
-#endif
 };
 
 struct core_thread {
