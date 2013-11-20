8a5c169447f4d7d0a504e10bfb053fefc7505cb7
DerTeufel <dominik-kassel@gmx.de>
2013-11-20 14:47:09 +0100

    much smaller kernel boot.img by compiling the modules (with the whole initramfs) into the zImage. just use a fakeramdisk to create standard boot.img
    Change-Id: I48f3d3c42939869e1110d76525607d3cb24e5e3e

--- build_combo.sh
+++ build_combo.sh
@@ -21,7 +21,7 @@ CUSTOM_PATH=note
 MODE=DUAL	
 fi
 
-displayversion=Devil2-1.0.15
+displayversion=Devil2-1.0.22
 version=$displayversion-$TARGET-$MODE-$(date +%Y%m%d)
 
 if [ -e boot.img ]; then
@@ -41,16 +41,13 @@ KERNEL_PATH=$PWD
 
 # Set toolchain and root filesystem path
 if [ "$(whoami)" == "dominik" ]; then
-	#TOOLCHAIN_PATH="/home/dominik/android/android_4.2/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin"
-	#TOOLCHAIN_PATH="/home/dominik/android/android_4.2/prebuilt/linux-x86/toolchain/android-toolchain-eabi-4.8-2013.07/bin"
 	TOOLCHAIN_PATH="/home/dominik/android/android_4.2/prebuilt/linux-x86/toolchain/android-toolchain-eabi-4.8-2013.09/bin"
-	#TOOLCHAIN_PATH="/home/dominik/android/android_4.2/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7.2/bin"
 elif [ "$(whoami)" == "rollus" ]; then
 	TOOLCHAIN_PATH="/home/rollus/android-toolchain-eabi/bin/"
 fi
 TOOLCHAIN="$TOOLCHAIN_PATH/arm-eabi-"
 ROOTFS_PATH="$KERNEL_PATH/ramdisks/$TARGET-combo"
-MODULES="$KERNEL_PATH/ramdisks/modules"
+MODULES="$ROOTFS_PATH/lib/modules"
 
 defconfig=cyanogenmod_"$TARGET"_defconfig
 
@@ -77,18 +74,22 @@ rm -rf $KERNEL_PATH/arch/arm/boot/zImage
 # Making our .config
 make $defconfig
 
-make -j`grep 'processor' /proc/cpuinfo | wc -l` ARCH=arm CROSS_COMPILE=$TOOLCHAIN || exit -1
+# make the modules
+make modules -j`grep 'processor' /proc/cpuinfo | wc -l` ARCH=arm CROSS_COMPILE=$TOOLCHAIN || exit -1
 # Copying and stripping kernel modules
 if [ "$TARGET" != "i9100" ] ; then
-mkdir -p $MODULES/lib/modules
-find -name '*.ko' -exec cp -av {} $MODULES/lib/modules/ \;
-        for i in $MODULES/lib/modules/*; do $TOOLCHAIN_PATH/arm-eabi-strip --strip-unneeded $i;done;\
+mkdir -p $MODULES
+find -name '*.ko' -exec cp -av {} $MODULES \;
+        for i in $MODULES/*; do $TOOLCHAIN_PATH/arm-eabi-strip --strip-unneeded $i;done;\
 else
 MODULES=releasetools/$CUSTOM_PATH/zip/system/lib/modules
 mkdir -p $MODULES
 find -name '*.ko' -exec cp -av {} $MODULES \;
         for i in $MODULES/*; do $TOOLCHAIN_PATH/arm-eabi-strip --strip-unneeded $i;done;\
 fi
+
+# make the kernel
+make zImage -j`grep 'processor' /proc/cpuinfo | wc -l` ARCH=arm CROSS_COMPILE=$TOOLCHAIN || exit -1
 
 # Copy Kernel Image
 rm -f $KERNEL_PATH/releasetools/$CUSTOM_PATH/tar/$version.tar
@@ -97,12 +98,13 @@ cp -f $KERNEL_PATH/arch/arm/boot/zImage .
 
 if [ "$TARGET" != "i9100" ] ; then
 # Create ramdisk.cpio archive
-cd $MODULES
-find . | cpio -o -H newc > $KERNEL_PATH/ramdisk.cpio
+cd $KERNEL_PATH/usr/fakeramdisk
+RAMDISK=ramdisk.cpio
+find . | cpio -o -H newc > $KERNEL_PATH/$RAMDISK
 cd $KERNEL_PATH
 
 # Make boot.img
-./mkbootimg --kernel zImage --ramdisk ramdisk.cpio --board smdk4x12 --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $KERNEL_PATH/boot.img
+./mkbootimg --kernel zImage --ramdisk $RAMDISK --board smdk4x12 --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $KERNEL_PATH/boot.img
 
 # Copy boot.img
 cp boot.img $KERNEL_PATH/releasetools/$CUSTOM_PATH/zip
