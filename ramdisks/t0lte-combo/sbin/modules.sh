be069cb1f41e1ae0e0839bc18f7dc70754a4f2bf
DerTeufel <dominik-kassel@gmx.de>
2013-12-30 07:55:10 +0100

    recovery part2
    Change-Id: I5fc55c9ecf225e2478bd8597aa6153b13068e0a0

--- ramdisks/t0lte-combo/sbin/modules.sh
+++ ramdisks/t0lte-combo/sbin/modules.sh
@@ -14,3 +14,15 @@ if [ -f /lib/modules/exfat_core.ko ] ; then
     insmod /lib/modules/exfat_core.ko
     insmod /lib/modules/exfat_fs.ko
 fi
+
+# usb connection workaround, neeed because of broken default.prop execution
+
+if [ -f /data/property/persist.sys.usb.config ] ; then
+	if grep -q mtp /data/property/persist.sys.usb.config; then
+		echo "mtp should be working already"
+	else
+		setprop persist.sys.usb.config mtp,adb
+	fi
+else
+	setprop persist.sys.usb.config mtp,adb
+fi 
