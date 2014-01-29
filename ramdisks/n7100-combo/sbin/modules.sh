261dda4883d6cb677211cab09a6bab9aadc1b604
DerTeufel <dominik-kassel@gmx.de>
2014-01-29 09:40:20 +0100

    n7100: f2fs bring up
    Change-Id: Ie8729d0362e295ffa10578115456fb3bd288a887

--- ramdisks/n7100-combo/sbin/modules.sh
+++ ramdisks/n7100-combo/sbin/modules.sh
@@ -1,7 +1,6 @@
-#!/sbin/busybox sh
-BB=/sbin/busybox
+#!/system/bin/sh
 
-$BB date >>modules.txt
+date >>modules.txt
 exec >>modules.txt 2>&1
 
 #  try ko files for exfat
@@ -15,15 +14,3 @@ if [ -f /lib/modules/exfat_core.ko ] ; then
     insmod /lib/modules/exfat_core.ko
     insmod /lib/modules/exfat_fs.ko
 fi
-
-# usb connection workaround, neeed because of broken default.prop execution
-
-if [ -f /data/property/persist.sys.usb.config ] ; then
-	if grep -q mtp /data/property/persist.sys.usb.config; then
-		echo "mtp should be working already"
-	else
-		setprop persist.sys.usb.config mtp,adb
-	fi
-else
-	setprop persist.sys.usb.config mtp,adb
-fi 
